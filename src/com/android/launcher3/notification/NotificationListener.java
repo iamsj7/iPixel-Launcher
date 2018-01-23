f6c14c5628951908add6a5db48d559bab8667288
Amir Zaidi <azaidi@live.nl>
2018-01-23 15:11:05 +0100

    PixelHome: Backport notification dots to Marshmallow and higher

--- src/com/android/launcher3/notification/NotificationListener.java
+++ src/com/android/launcher3/notification/NotificationListener.java
@@ -34,6 +34,7 @@ import android.util.Log;
 import android.util.Pair;
 
 import com.android.launcher3.LauncherModel;
+import com.android.launcher3.Utilities;
 import com.android.launcher3.util.PackageUserKey;
 import com.android.launcher3.util.SettingsObserver;
 
@@ -363,17 +364,16 @@ public class NotificationListener extends NotificationListenerService {
 
     private boolean shouldBeFilteredOut(StatusBarNotification sbn) {
         Notification notification = sbn.getNotification();
-
-        updateGroupKeyIfNecessary(sbn);
-
-        getCurrentRanking().getRanking(sbn.getKey(), mTempRanking);
-        if (!mTempRanking.canShowBadge()) {
-            return true;
-        }
-        if (mTempRanking.getChannel().getId().equals(NotificationChannel.DEFAULT_CHANNEL_ID)) {
-            // Special filtering for the default, legacy "Miscellaneous" channel.
-            if ((notification.flags & Notification.FLAG_ONGOING_EVENT) != 0) {
+        if (Utilities.ATLEAST_OREO) {
+            getCurrentRanking().getRanking(sbn.getKey(), mTempRanking);
+            if (!mTempRanking.canShowBadge()) {
                 return true;
+            }
+            if (mTempRanking.getChannel().getId().equals(NotificationChannel.DEFAULT_CHANNEL_ID)) {
+                // Special filtering for the default, legacy "Miscellaneous" channel.
+                if ((notification.flags & Notification.FLAG_ONGOING_EVENT) != 0) {
+                    return true;
+                }
             }
         }
 
